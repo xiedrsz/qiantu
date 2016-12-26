94d4f22bac76453745151c5de25d82699c4b293b
xiedairong <xiedrsz@sinosoft.com.cn>
2016-12-26 18:17:02 +0800

    侧滑菜单，转场效果

--- config/main.js
+++ config/main.js
@@ -1,37 +1,51 @@
 requirejs.config({
-    //By default load any module IDs from js/lib
-    baseUrl: './',
-    //except, if the module ID starts with "app",
-    //load it from the js/app directory. paths
-    //config is relative to the baseUrl, and
-    //never includes a ".js" extension since
-    //the paths config could be for a directory.
-    paths: {
-        text: './libs/require/text',
-        xvue: './libs/require/xvue',
-        Vue: './libs/vue/vue',
-        filter: './libs/vue/filter',
-        mock: './libs/mock-min',
-        analyzer: './libs/analyzer',
-        dataApi: './libs/dataApi',
-        VueRouter: './libs/vue/vue-router',
-        resource: './libs/vue/vue-resource.min',
-        ext: './libs/extend.min',
-        router: './config/router',
-        setting: './config/setting',
-        mockData: './mocks/mockData',
-    },
-    shim: {
-        VueRouter: ['Vue']
-    }
+	//By default load any module IDs from js/lib
+	baseUrl: './',
+	//except, if the module ID starts with "app",
+	//load it from the js/app directory. paths
+	//config is relative to the baseUrl, and
+	//never includes a ".js" extension since
+	//the paths config could be for a directory.
+	paths: {
+		text: './libs/require/text',
+		xvue: './libs/require/xvue',
+		Vue: './libs/vue/vue',
+		filter: './libs/vue/filter',
+		VueRouterTransition: './libs/vue/vue-router-transition.min',
+		mock: './libs/mock-min',
+		analyzer: './libs/analyzer',
+		dataApi: './libs/dataApi',
+		VueRouter: './libs/vue/vue-router',
+		resource: './libs/vue/vue-resource.min',
+		ext: './libs/extend.min',
+		router: './config/router',
+		setting: './config/setting',
+		mockData: './mocks/mockData'
+	},
+	shim: {
+		VueRouter: ['Vue']
+	}
 });
 
 // Start the main app logic.
 requirejs(['Vue', 'router', 'resource', 'setting', 'filter'], function (Vue, router, resource) {
-    Vue.use(resource);
-    // 以表单形式发送数据
-    Vue.http.options.emulateJSON = true;
-    /* 启动路由  */
-    var App = Vue.extend({})
-    router.start(App, '#app')
+	Vue.use(resource);
+	// 以表单形式发送数据
+	Vue.http.options.emulateJSON = true;
+	/* 启动路由  */
+	var App = Vue.extend({
+		data: function () {
+			return {
+				menuClass: "",
+				routeTran: "vux-pop-out"
+			}
+		},
+		methods: {
+			closeMenu: function () {
+				this.menuClass = "menu_animation1";
+			}
+		}
+	});
+
+	router.start(App, '#app');
 });
--- config/router.js
+++ config/router.js
@@ -1,24 +1,25 @@
-define(['Vue', 'VueRouter', 'xvue!./modules/home.xvue', 'xvue!./modules/account.xvue'], function (Vue, VueRouter, Home, Account) {
-    'use strict';
+define(['Vue', 'VueRouterTransition', 'VueRouter', 'xvue!./modules/home.xvue', 'xvue!./modules/account.xvue'], function (Vue, VueRouterTransition, VueRouter, Home, Account) {
+	'use strict';
 
-    Vue.use(VueRouter);
+	Vue.use(VueRouter);
+	Vue.use(VueRouterTransition, VueRouter)
 
-    var router = new VueRouter();
+	var router = new VueRouter();
 
-    /* 创建路由映射  */
-    router.map({
-        '/home': {
-            component: Home
-        },
-        '/account/:bill': {
-            name: 'account',
-            component: Account
-        }
-    });
+	/* 创建路由映射  */
+	router.map({
+		'/home': {
+			component: Home
+		},
+		'/account/:bill': {
+			name: 'account',
+			component: Account
+		}
+	});
 
-    router.redirect({
-        '/': '/home'
-    });
+	router.redirect({
+		'/': '/home'
+	});
 
-    return router;
+	return router;
 });
