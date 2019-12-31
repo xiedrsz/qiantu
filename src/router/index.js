d61b152a118566649051fabc78e960db7bf9b7ed
谢代荣 <xiedrsz@sinosoft.com.cn>
2019-12-31 16:31:53 +0800

    清理

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Collection from '../views/Collection.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
+    name: 'Collection',
+    component: Collection
   },
   {
     path: '/backup',
@@ -95,13 +85,6 @@ const routes = [
     name: 'Account',
     component: function () {
       return import(/* webpackChunkName: "account" */ '../views/Account.vue')
-    }
-  },
-  {
-    path: '/collection',
-    name: 'Collection',
-    component: function () {
-      return import(/* webpackChunkName: "collection" */ '../views/Collection.vue')
     }
   },
   {
